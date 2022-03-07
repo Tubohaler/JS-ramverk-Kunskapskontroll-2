643637c7d6818b2c63619c5fdb8a5d2fa6767f59
David Skotte <david.skotte@gmail.com>
2022-03-07 17:01:41 +0100

    första commit, basics down

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Todo from './components/Todo.vue';
+</script>
+
+<template>
+ <Todo />
+ <!-- <img :class="background-img" alt="Background" src="./assets/background.jpeg" />
+ <img :class="colorOverlay" alt="bakground overlay" src="./assets/background.jpeg" /> -->
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: -25px;
+  background-color: rgb(255, 255, 255);
+}
+
+.background-img {
+  position: absolute;
+  margin-left: 24%;
+  height: 90%;
+  object-fit: cover;
+  overflow: hidden;
+}
+
+.colorOverlay {
+  background: #000;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  opacity: 0.7;
+  object-fit: cover;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+const todoEmpty = ref("")
+const addNewTodo = ref([
+    {title: "Sporta mer", complete: false }, 
+    {title: "Sporta mer", complete: false },
+])
+const deleteTodo = ref([])
+;
+
+</script>
+
+<template>
+  <h1>David's Todo-Shite!</h1>
+  <label>Do something!</label>
+  <div>
+    <input name="newTodo" v-model="todoEmpty" />
+    <button @click="addNewTodo" >Add</button>
+    <button @click="deleteTodo">Kill</button>
+  </div>
+
+  <ul>
+    <li v-for="(todoEmpty, index) in todoEmpty" :key="index">{{addNewTodo.title}}</li>
+  </ul>
+</template>
+
+
+
+
+<style>
+</style>
