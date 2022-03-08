19070b56e3fa2397e748a61dce59e1c25871828a
David Skotte <david.skotte@gmail.com>
2022-03-08 15:41:56 +0100

    bakgrund aktiverad

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@ import Todo from './components/Todo.vue';
 
 <template>
  <Todo />
- <!-- <img :class="background-img" alt="Background" src="./assets/background.jpeg" />
- <img :class="colorOverlay" alt="bakground overlay" src="./assets/background.jpeg" /> -->
-
 </template>
 
 <style>
@@ -15,25 +12,9 @@ import Todo from './components/Todo.vue';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin: -25px;
-  background-color: rgb(255, 255, 255);
+  color: #e0e3e6;
+  margin: 4rem;
+  background-color: rgb(0, 0, 0);
 }
 
-.background-img {
-  position: absolute;
-  margin-left: 24%;
-  height: 90%;
-  object-fit: cover;
-  overflow: hidden;
-}
-
-.colorOverlay {
-  background: #000;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  opacity: 0.7;
-  object-fit: cover;
-}
 </style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,7 +2,7 @@
 import { ref } from "vue";
 
 const todoEmpty = ref("");
-const addNewTodos = ref([{ title: "", complete: false }]);
+const addNewTodos = ref([{ title:"", complete: false }]);
 const deleteTodo = ref([]);
 
 function pushingTodos() {
@@ -17,31 +17,88 @@ function deleteActivity(index) {
   addNewTodos.value.splice(index, 1);
 }
 
+function completedTodos(addNewTodos) {
+  index.value.forEach((addNewTodos, index) => {
+    if (index === addNewTodos) {
+      addNewTodos.complete = !addNewTodos.complete;
+    }
+  })
+}
+  
+
 </script>
 
 <template>
-  <h1>David's Todo-Shite!</h1>
+  <h1 :class="title" >David's Todo-Shite!</h1>
+  
+  <img :class="colorOverlay" alt="bakground overlay" src="../assets/background.jpeg" />
+  <img :class="background-img" alt="Background" src="../assets/background.jpeg" />
+  
   <label>Do something!</label>
-  <div>
+  <div class="list">
     <input name="newTodo" v-model="todoEmpty" />
     <button @click="pushingTodos">Add</button>
     <button @click="deleteActivity(index)">Kill</button>
   </div>
 
-  <ul :class="{ listItems }">
-    <li v-for="(addNewTodos, index) in addNewTodos" :key="index">
+  <div class="list">
+    <div v-for="(addNewTodos, index) in addNewTodos" :class="listItem" :key="index" >
+        <span :class="{ completed: addNewTodos.complete }" >{{addNewTodos.title}} </span>
       {{ addNewTodos.title }}
-      <input type="checkbox"/>
-    </li>
-  </ul>
+      <input :class="{complete: index.complete }" @click="completedTodos(index)" type="checkbox"/>
+    </div>
+  </div>
 </template>
 
 <style>
-.listItems {
-  text-decoration: none;
+html {
+    background-color: #000;
 }
 
-.done {
-  text-decoration: line-through;
+.list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
+
+.listItem {
+   height: 60px;
+   width: 240px;
+   margin-top: 12px;
+   border: 3px solid rgb(255, 255, 255);
+
+}
+
+
+/* .title {
+  font-family: 'MedievalSharp', cursive;
+  color: #ffffff;
+  z-index: 2;
+  margin: 0.2rem;
+  margin-top: 8rem;
+  padding: 0rem;
+} */
+
+.completed {
+    text-decoration: line-through;
+}
+
+/* .background-img {
+  position: absolute;
+  margin-left: 24%;
+  height: 90%;
+  object-fit: cover;
+  overflow: hidden;
+  z-index: 0;
+}
+
+.colorOverlay {
+  background: #000;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  opacity: 0.7;
+  object-fit: cover;
+  z-index: 1;
+} */
 </style>
