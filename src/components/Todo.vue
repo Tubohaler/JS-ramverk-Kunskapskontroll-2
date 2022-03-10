c4e7cdb85e2ca7b87915cbfc1c6994475c169046
David Skotte <david.skotte@gmail.com>
2022-03-10 15:07:41 +0100

    klar?  - fråga om feedback

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -30,21 +30,15 @@ function completedTodos(addNewTodos) {
   <h1 class="background">David's Todo-Shite!</h1>
 
   <label>Do something!</label>
-  <div class="buttonChoices">
-    <input name="newTodo" v-model="todoEmpty" />
-    <button @click="pushingTodos">Add</button>
-    <button @click="deleteActivity(index)">Kill</button>
+  <div class="todo-container">
+    <input class="form-input" name="newTodo" v-model="todoEmpty" />
+    <button class="complete-btn" @click="pushingTodos">Add</button>
+    <button class="trash-btn" @click="deleteActivity(index)">Kill</button>
   </div>
 
   <div class="list">
-    <div
-      v-for="(todo, index) in addNewTodos"
-      class="listItem"
-      :key="index"
-    >
-      <span :class="{ completed: todo.complete }"
-        >{{ todo.title }}
-      </span>
+    <div v-for="(todo, index) in addNewTodos" class="listItem" :key="index">
+      <span :class="{ completed: todo.complete }">{{ todo.title }} </span>
       <input
         :class="{ complete: index.complete }"
         @click="completedTodos(index)"
@@ -55,9 +49,30 @@ function completedTodos(addNewTodos) {
 </template>
 
 <style>
+.background {
+  color: gold;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  font-family: fantasy;
+  font-size: xxx-large;
+  margin: -1rem;
+  z-index: 0;
+}
+
+.backgroundImg {
+  height: 75vh;
+  position: absolute;
+  opacity: 0.1;
+  margin-top: -2rem;
+}
 
 label {
-  margin-left: -118px;
+  margin-left: 0px;
+  font-size: xx-large;
+  font-family: cursive;
+  color: goldenrod;
 }
 
 .list {
@@ -68,36 +83,59 @@ label {
 
 .listItem {
   height: 25px;
-  width: 223px;
-  margin-top: 16px;
   border: 3px solid rgb(255, 255, 255);
+  margin: 0.5rem;
+  background: white;
+  font-size: 1.5rem;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 31.9%;
+  list-style: none;
+  margin-left: 3rem;
+  margin-top: 0.7rem;
+  z-index: 3;
 }
 
 input {
   padding: 0px;
-
 }
 
 button {
   border: 0px;
   background-color: orange;
-  margin: 0.1rem;
+  margin: 0rem;
 }
 
-.background {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  z-index: 0;
+.form-input,
+form button {
+  font-size: 2.73rem;
+  border: none;
+  z-index: 2;
+}
+form button {
+  color: #ff6f47;
+  background: #f7fffe;
+  cursor: pointer;
+  transition: all 0.3s ease;
 }
 
-.backgroundImg {
-  height: 75vh;
-  position: absolute;
-  opacity: 0.1;
-  margin-top: -2rem;
+.trash-btn,
+.complete-btn {
+  background: #ff6f47;
+  color: white;
+  border: none;
+  padding: 1rem;
+  cursor: pointer;
+  font-size: 1rem;
 }
+
+.complete-btn {
+  background: rgb(11, 212, 162);
+}
+
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css ");
 
 /* .title {
   font-family: 'MedievalSharp', cursive;
@@ -110,5 +148,27 @@ button {
 
 .completed {
   text-decoration: line-through;
+}
+
+.todo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2vw;
+}
+
+.todo {
+  margin: 0.5rem;
+  background: white;
+  font-size: 1.5rem;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: all 1s ease;
+  width: 70%;
+  min-width: 30%;
+  list-style: none;
+  z-index: 3;
 }
 </style>
